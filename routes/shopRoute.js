228cf960e3a7c0542622245ce4155a09caccb764
Trung-2001140053 <118284282+Trung-2001140053@users.noreply.github.com>
2023-03-27 20:37:43 +0700

    Add files via upload

---
+++ routes/shopRoute.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const shopController = require("../controllers/shopController");
+
+// Home
+router.get("/home", shopController.renderHomePage);
+router.post("/home", shopController.handleUpload);
+
+// Prodct
+router.get("/product", shopController.renderProductPage);
+
+router.get("/login", shopController.renderLoginPage);
+router.get("/about", shopController.renderAboutPage);
+router.get("/my-form-handler", shopController.handleTest);
+router.post("/my-form-handler", shopController.handleAdminAccount);
+module.exports = router;
